db37585254570d5a266558c4812682dcfd867c3c
Jens W. Klein <jk@kleinundpartner.at>
2024-06-17 13:30:35 +0200

    jssi projen used

---
+++ src/backend.ts
@@ -0,0 +1,23 @@
+import { Construct } from 'constructs';
+import { PloneBackendDeploymentOptions, PloneBackendDeployment } from './backend-deployment';
+import { PloneBackendServiceOptions, PloneBackendService } from './backend-service';
+
+export interface PloneBackendOptions {
+  deployment?: PloneBackendDeploymentOptions;
+  service?: PloneBackendServiceOptions;
+}
+
+export class PloneBackend extends Construct {
+
+  constructor(scope: Construct, id: string, options: PloneBackendOptions = {}) {
+    super(scope, id);
+    const deployment = options.deployment ?? {};
+    const service = options.service ?? {};
+
+    // Create a deployment
+    new PloneBackendDeployment(this, 'deployment', deployment);
+
+    // Create a service
+    new PloneBackendService(this, 'service', service);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
-}
+export * from './backend-deployment';
+export * from './backend-service';
+export * from './backend';
