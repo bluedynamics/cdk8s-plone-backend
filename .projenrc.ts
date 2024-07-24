512be13fa9b6538d8a5673d0028a8faf576fa2ce
Jens W. Klein <jk@kleinundpartner.at>
2024-07-24 16:23:14 +0200

    fix name/repo url

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,9 +6,9 @@ const project = new cdk.JsiiProject({
   authorAddress: 'jk@kleinundpartner.at',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
-  name: '@bluedynamics/cdk8s-plone',
+  name: 'cdk8s-plone',
   projenrcTs: true,
-  repositoryUrl: 'git@github.com:bluedynamics/cdk8s-plone.git',
+  repositoryUrl: 'https://github.com/bluedynamics/cdk8s-plone.git',
   description: 'Provides a CMS Plone Backend and Frontend for Kubernetes with cdk8s',
   deps: [
     'cdk8s',
